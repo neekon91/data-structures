e355f148c6681d4c9ec208eca31b764fe29bba25
Neekon <neekon91@gmail.com>
2016-11-10 15:51:19 -0800

    func queue

--- Stack-Queue/functional/queue.js
+++ Stack-Queue/functional/queue.js
@@ -3,16 +3,25 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var first = 0;
+  var last = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[last] = value;
+    last++;
   };
 
   someInstance.dequeue = function() {
+    if(last - first > 0){
+      var remove = storage[first];
+      first++;
+      return remove;
+    }
   };
 
   someInstance.size = function() {
+    return last - first;
   };
 
   return someInstance;
