16fb01178cb4e69f546d4b56ab74d04b67e9110b
Neekon <neekon91@gmail.com>
2016-11-10 20:50:29 -0800

    bst

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -1,7 +1,21 @@
 var BinarySearchTree = function(value) {
+  var binaryTree = Object.create(treeMethods)
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  binaryTree.root = arguments[1] || false;
+  return binaryTree;
 };
+treeMethods.prototype.insert = function(value){
 
-
+}
+treeMethods.prototype.contains = function(){}
+treeMethods.prototype.depthFirstSearch = function(){}
+treeMethods.prototype.breadthFirstLog = function(){}
+treeMethods.prototype.balanceFactor = function(){}
+treeMethods.prototype.rotateRight = function(){}
+treeMethods.prototype.rotateLeft = function(){}
+treeMethods.prototype.rebalance = function(){}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
