6bac4562bf22d96e42e6bc3d9b82996e1265da6d
Neekon <neekon91@gmail.com>
2016-11-10 19:09:17 -0800

    tree contains and addshild complete

--- data-structures/tree.js
+++ data-structures/tree.js
@@ -1,23 +1,28 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  newTree.parent = null;
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
-  // your code here
-  newTree.children = null;  // fix me
+  this.children.push(Tree(value));
+  return true;
 };
 
 treeMethods.contains = function(target) {
+  return (this.value === target) ? true :
+    this.children.some(child => child.contains(target));
 };
 
+treeMethods.removeFromParent = function(){
+
+};
 
 
 /*
