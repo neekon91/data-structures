f9ca7232f98c2ee3dfc6101f30c44d15a73234b5
Neekon <neekon91@gmail.com>
2016-11-10 21:08:30 -0800

    insert

--- data-structures/binarySearchTree.js
+++ data-structures/binarySearchTree.js
@@ -1,21 +1,33 @@
 var BinarySearchTree = function(value) {
-  var binaryTree = Object.create(treeMethods)
+  var binaryTree = Object.create(bianryMethod);
   binaryTree.value = value;
   binaryTree.left = null;
   binaryTree.right = null;
-  binaryTree.root = arguments[1] || false;
   return binaryTree;
 };
-treeMethods.prototype.insert = function(value){
 
-}
-treeMethods.prototype.contains = function(){}
-treeMethods.prototype.depthFirstSearch = function(){}
-treeMethods.prototype.breadthFirstLog = function(){}
-treeMethods.prototype.balanceFactor = function(){}
-treeMethods.prototype.rotateRight = function(){}
-treeMethods.prototype.rotateLeft = function(){}
-treeMethods.prototype.rebalance = function(){}
+var bianryMethod = {
+  insert: function(value){
+    if(this.value > value){
+      (!this.left) ? this.left = BinarySearchTree(value) : this.left.insert(value);
+    } else if(this.value < value){
+      (!this.right) ? this.right = BinarySearchTree(value) : this.right.insert(value);
+    }
+  },
+  contains: function(target){
+
+  },
+  depthFirstLog: function(callback){
+
+  }
+};
+// treeMethods.prototype.contains = function(){}
+// treeMethods.prototype.depthFirstLog = function(){}
+// treeMethods.prototype.breadthFirstLog = function(){}
+// treeMethods.prototype.balanceFactor = function(){}
+// treeMethods.prototype.rotateRight = function(){}
+// treeMethods.prototype.rotateLeft = function(){}
+// treeMethods.prototype.rebalance = function(){}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
