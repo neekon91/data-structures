6b470490011a151a75e2f7e762049bf88d112171
Neekon <neekon91@gmail.com>
2016-11-10 18:30:02 -0800

    linked list

--- data-structures/linkedList.js
+++ data-structures/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(!this.head){
+      this.head = new Node(value)
+      this.tail = this.head;
+    } else {
+      this.tail.next = new Node(value);
+      this.tail = this.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    if(this.head){
+      var remove = this.head.value;
+      this.head = this.head.next;
+      return remove;
+    }
   };
 
   list.contains = function(target) {
+    var cur = this.head;
+    while(cur){
+      if(cur.value === target){
+        return true;
+      }
+      cur = cur.next;
+    }
+    return false;
   };
 
   return list;
