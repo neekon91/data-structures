2171aaf431dd2957d540e081d7253319bab72e15
Neekon <neekon91@gmail.com>
2016-11-10 18:08:20 -0800

    stack pseudoclassical

--- Stack-Queue/pseudoclassical/stack.js
+++ Stack-Queue/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
 
+Stack.prototype.push = function(value){
+  this.storage[++this.count] = value;
+},
+Stack.prototype.pop = function(){
+  if(this.count){
+    var remove = this.storage[this.count];
+    delete this.storage[this.count--];
+    return remove;
+  }
+},
+Stack.prototype.size = function(){
+  return this.count;
+}
