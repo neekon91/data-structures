6838db2199a10beb01f7a2a4729a967b1366cc6a
Neekon <neekon91@gmail.com>
2016-11-10 18:02:22 -0800

    pseudo queue

--- Stack-Queue/pseudoclassical/queue.js
+++ Stack-Queue/pseudoclassical/queue.js
@@ -1,6 +1,18 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.first = 0;
+  this.last = 0;
+  this.storage = {};
 };
-
-
+Queue.prototype.enqueue = function(value){
+  this.storage[this.last++] = value;
+};
+Queue.prototype.dequeue = function(){
+  if(this.last - this.first > 0){
+    var remove = this.storage[this.first];
+    delete this.storage[this.first++];
+    return remove;
+  }
+}
+Queue.prototype.size = function(){
+  return this.last - this.first;
+}
